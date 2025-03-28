b85e850c98c141d48216143cf46bc958f6ed62c1
salma <>
2025-03-28 01:14:35 +0000

    second commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,27 +19,28 @@ return new class extends Migration
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('cin');
-            $table->string('role');
+            $table->enum('role',['admin','agent'])->default('agent');
             $table->string('tel');
 
             $table->rememberToken();
             $table->timestamps();
+        
         });
 
-    //     Schema::create('password_reset_tokens', function (Blueprint $table) {
-    //         $table->string('E-mail')->primary();
-    //         $table->string('token');
-    //         $table->timestamp('created_at')->nullable();
-    //     });
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('E-mail')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
 
-    //     Schema::create('sessions', function (Blueprint $table) {
-    //         $table->string('id')->primary();
-    //         $table->foreignId('user_id')->nullable()->index();
-    //         $table->string('ip_address', 45)->nullable();
-    //         $table->text('user_agent')->nullable();
-    //         $table->longText('payload');
-    //         $table->integer('last_activity')->index();
-    //     });
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+         });
      }
 
     /**
